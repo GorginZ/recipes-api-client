ec5d9de15f5510aa00714bc06dfe1e372c79dd1b
GorginZ <georgia.z.leng@gmail.com>
2020-07-05 20:46:17 +1000

    still not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,65 @@
 import React from "react";
 import Recipes from "./components/Recipes";
-import Search from "./components/Search";
-
-
-
-// 9. Add a new function in <App /> named searchRecipes() it should take a search as a parameter, you then need to pass this function as a prop through <Search /> and <Form />
-
+// import Search from "./components/Search";
 class App extends React.Component {
-searchRecipes =(search)=>{
-  console.log("search from searchRecipes " + search)
-}
-    render() {
-        return (
-          <>
-            <div className="App">
-               <Search search={this.props} />
-               <Recipes />
-            </div>
-            </>
-        );
-    }
+  state = {
+    recipes: []
+  } 
+
+  async componentDidMount() {
+    const response = await fetch("http://localhost:3000/recipes")
+    const data = await response.json()
+    this.setState({
+    recipes: data
+  })
 }
 
+  render() {
+    return this.state.recipes.map((recipe, index) => {
+      return (
+          <Recipes key={index} name={recipe.name}
+          ingredients={recipe.ingredients}
+          method={recipe.method} />
+      )
+    })
+  }
+}
+//     const data = [
+//       {
+//         name: "Chocolate cake",
+//         cooking_time: "2 hours",
+//         method: [
+//           'Mix all the ingredients together',
+//           'Add the egg',
+//           'Put in the oven'
+//         ],
+//         ingredients: [
+//           'egg',
+//           'sugar',
+//           'butter'
+//         ]
+//       },
+//       {
+//         name: "Pumpkin soup",
+//         cooking_time: "30 mins",
+//         method: [
+//           'Chop Pumpkin',
+//           'cook pumpkin real good'
+//         ],
+//       ingredients: [
+//         'Punpkin'
+//       ]
+//       }
+//     ]
+//     return data.map((recipes) => {
+//       return (
+//        <Recipes name={recipes.name}
+//        cooking_time={recipes.cooking_time}
+//        method={recipes.method}
+//         />
+//       )
+//     })
+//   }
+// }
+      
 export default App;
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 
 class Recipes extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>on recipes</h1>
-            </div>
-        );
-    }
+   render() {
+       const { name, method, ingredients } = this.props; //destructuring 
+       const methodArr = JSON.parse(method)
+       const ingredientsArr = JSON.parse(ingredients)
+       return (
+           <>
+        <h1>{name}</h1>
+           <h4>Ingredients</h4>
+           <ul>
+               {ingredientsArr.map((ingredient, index) => {
+                   return <li key={index}> {ingredient}</li>
+               })}
+           </ul>
+
+           <li>
+               {methodArr.map((method, index) => {
+                   return <li key={index}> {method}</li>
+               })}
+           </li>
+           <hr/>
+           
+       </>
+       )
+
+   }
 }
 
+
+
+
+
+
+
 export default Recipes;
+
+//for deleting and editing buttons 
+// <div className="edit-delete-container">
+// <Link to={`/recipes/${recipe.id}/edit`}>Edit</Link>
+// <span onClick={() => this.deleteRecipe(recipe.id)}>Delete</span>
+// </div>
